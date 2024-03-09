93457d9349604245d0befb37f33c5fcd32d187a9
R29-dev <congdoan.29042004@gmail.com>
2024-03-09 21:17:01 +0700

    ds

--- resources/views/Frontend/home/index.blade.php
+++ resources/views/Frontend/home/index.blade.php
@@ -513,13 +513,13 @@
                             <div class="product-image-wrapper">
                                 <div class="single-products">
                                     <div class="productinfo text-center">
-                                        <img src=" {{ asset('frontend/images/home/recommend2.jpg"') }} alt="" />
+                                        <img src=" {{ asset('frontend/images/home/recommend2.jpg') }}" alt="" />
                                         <h2>$56</h2>
                                         <p>Easy Polo Black Edition</p>
                                         <a href="#" class="btn btn-default add-to-cart"><i
                                                 class="fa fa-shopping-cart"></i>Add to cart</a>
                                     </div>
-                                   
+                                    {{ asset('frontend/') }}
                                 </div>
                             </div>
                         </div>
